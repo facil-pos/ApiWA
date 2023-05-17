75c7a43de85ee5e6358ca6659a42d4b7701bc4d9
Nico <vassallo.mn@gmail.com>
2023-05-17 15:42:26 -0300

    cambios en index

--- index.js
+++ index.js
@@ -22,43 +22,6 @@ app.use(session({
 app.use('/', routes); // Rutas de la API
 
 generateQrCodes(clients);
-/* app.get('/qrcode/:clientId', requireLogin, async (req, res) => {
-    const clientId = req.params.clientId;
-    const checkDuplicateQuery = {
-        text: 'SELECT COUNT(*) FROM users WHERE client = $1',
-        values: [clientId],
-    };
-
-    try {
-        const result = await pool.query(checkDuplicateQuery);
-        const duplicateCount = result.rows[0].count;
-
-        if (duplicateCount > 0) {
-            const clearPreviousClientQuery = {
-                text: 'UPDATE users SET client = NULL, updated_at = NOW() WHERE client = $1',
-                values: [clientId],
-            };
-            await pool.query(clearPreviousClientQuery);
-        }
-        const updateClientQuery = {
-            text: 'UPDATE users SET client = $1, updated_at = NOW() WHERE username = $2',
-            values: [clientId, req.session?.user?.username],
-        };
-        await pool.query(updateClientQuery);
-        console.log(`Cliente ${clientId} actualizado para el usuario ${req.session?.user?.username}`);
-
-        const qrImage = await getQrImage(clientId);
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': qrImage.length,
-        });
-        res.end(qrImage);
-    } catch (error) {
-        console.error(`Error al actualizar el cliente para el usuario ${req.session?.user?.username}: ${error}`);
-        res.sendStatus(500);
-    }
-}); */
-
 app.post('/qrcode', requireLogin, async (req, res) => {
     const clientId = req.body.clientId; // Obtener el clientId desde el cuerpo de la solicitud
 
